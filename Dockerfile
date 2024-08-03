eb1392b893baa27c950c6b6727ae72741d4f3484
abhishek <abhisheksara21@gmail.com>
2024-08-03 20:45:37 +0530

    get user

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 from python:3.10-alpine
 workdir /usr/src/app
-copy app/* .
-copy ./requirements.txt .
+copy app/main.py .
+copy ./requirements.txt ./requirements.txt
 run pip install -r requirements.txt
 cmd ["python","./main.py"]
--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ app = FastAPI(
 async def hello_world():
     return {"message":"Hello World!"}
 
+@app.get("/user/{user_id}")
+async def hello_user(user_id):
+    return {"message":f"hello {user_id}"}
+
 if __name__=="__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=8000)
