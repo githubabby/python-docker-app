3bb94265feecaa5ec0d7a7cb54b3ca08d171091c
abhishek <abhisheksara21@gmail.com>
2024-08-05 20:55:26 +0530

    adding basic logging!

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+import logging
+logger = logging.getLogger('SimpleLog')
+logging.basicConfig(
+    filename='simple.log',
+    encoding='utf-8',
+    format='%(asctime)s :%(name)s:%(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+    level=logging.DEBUG
+)
+
+
 from fastapi import FastAPI
 
 app = FastAPI(
@@ -8,10 +19,12 @@ app = FastAPI(
 
 @app.get("/")
 async def hello_world():
+    logger.info("hello world is called!")
     return {"message":"Hello World!"}
 
 @app.get("/user/{user_id}")
 async def hello_user(user_id):
+    logger.info(f"hello world with {user_id=} is called!")
     return {"message":f"hello {user_id}"}
 
 if __name__=="__main__":
